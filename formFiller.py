650d713e7d98ef7e19254969aa8f8fefea1740ee
Meghan McClain <61612006+meghanmcclain@users.noreply.github.com>
2022-03-07 22:11:14 -0600

    Create formFiller.py

---
+++ formFiller.py
@@ -0,0 +1,71 @@
+#! python3
+# formFiller.py - Automatically fills in the form.
+#have url open in browser: https://urldefense.com/v3/__https://docs.google.com/forms/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform__;!!DZ3fjg!ujOqpK9uJhNFogT8VTLi681jlBTdclV4Jb4rkWvWyfxbnLQGT6odT6d2VGc45kQ$ 
+
+import pyautogui, time
+
+formData = [{'name': 'Alice', 'fear': 'eavesdroppers', 'source': 'wand',
+            'robocop': 4, 'comments': 'Tell Bob I said hi.'},
+            {'name': 'Bob', 'fear': 'bees', 'source': 'amulet', 'robocop': 4,
+            'comments': 'n/a'},
+            {'name': 'Carol', 'fear': 'puppets', 'source': 'crystal ball',
+            'robocop': 1, 'comments': 'Please take the puppets out of the break room.'},
+            {'name': 'Alex Murphy', 'fear': 'ED-209', 'source': 'money',
+            'robocop': 5, 'comments': 'Protect the innocent. Serve the public trust. Uphold the law.'},
+           ] #list of 4 dictionaries
+
+submitAnotherLink = (-1184, 20) #find using pyautogui.mouseInfo()
+
+pyautogui.PAUSE = 0.5
+print('Ensure that the browser window is active and the form is loaded!')
+
+for person in formData:
+    # Give the user a chance to kill the script.
+    print('>>> 5-SECOND PAUSE TO LET USER PRESS CTRL-C <<<')
+    time.sleep(5)
+
+    # Wait until the form page has loaded.
+    print('Entering %s info...' % (person['name']))
+    pyautogui.write(['\t', '\t', '\t', '\t'])
+
+    # Fill out the Name Field.
+    pyautogui.write(person['name'] + '\t')
+
+    # Fill out the Greatest Fear(s) field.
+    pyautogui.write(person['fear'] + '\t')
+
+    # Fill out the Source of Wizard Powers field.
+    if person['source'] == 'wand':
+        pyautogui.write(['down', 'enter','\t'] , 0.5)
+    elif person['source'] == 'amulet':
+        pyautogui.write(['down', 'down', 'enter', '\t'] , 0.5)
+    elif person['source'] == 'crystal ball':
+        pyautogui.write(['down', 'down', 'down', 'enter', '\t'] , 0.5)
+    elif person['source'] == 'money':
+        pyautogui.write(['down', 'down', 'down', 'down', 'enter', '\t'] , 0.5)
+
+    # Fill out the RoboCop field.
+    if person['robocop'] == 1:
+        pyautogui.write([' ', '\t', '\t'] , 0.5)
+    elif person['robocop'] == 2:
+        pyautogui.write(['right', '\t', '\t'] , 0.5)
+    elif person['robocop'] == 3:
+        pyautogui.write(['right', 'right', '\t', '\t'] , 0.5)
+    elif person['robocop'] == 4:
+        pyautogui.write(['right', 'right', 'right', '\t', '\t'] , 0.5)
+    elif person['robocop'] == 5:
+        pyautogui.write(['right', 'right', 'right', 'right', '\t', '\t'] , 0.5)
+
+    # Fill out the Additional Comments field.
+    pyautogui.write(person['comments'] + '\t')
+
+    # Click Submit.
+    time.sleep(0.5) # Wait for the button to activate.
+    pyautogui.press('enter')
+
+    # Wait until form page has loaded.
+    print('Submitted form.')
+    time.sleep(5)
+
+    # Click the Submit another response link.
+    pyautogui.click(submitAnotherLink[0], submitAnotherLink[1])
